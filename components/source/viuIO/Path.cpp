4c892ba458350face4388159f4cf73db00c871be
qinhao1977@gmail.com <qinhao1977@gmail.com@ed219583-98d9-77fa-b7bf-0eab6e057094>
2012-08-29 21:52:07 +0000

    minor change

--- components/source/viuIO/Path.cpp
+++ components/source/viuIO/Path.cpp
@@ -69,24 +69,24 @@ namespace viu2x {
             // Make host section
             wstring hostSection = L"";
             hostSection = entity->Host;
-            if (entity->Username.size() > 0)
+            if (entity->Username.length() > 0)
             {
-                if (entity->Password.size() > 0)
+                if (entity->Password.length() > 0)
                     hostSection = HostDelimiter + entity->Username + PasswordDelimiter + entity->Password + UserDelimiter + hostSection;
                 else hostSection = HostDelimiter + entity->Username + UserDelimiter + hostSection;
             }
             // Attach host section without host delimiter
-            if (hostSection.size() > 0)
+            if (hostSection.length() > 0)
                 entityPath = hostSection + FolderDelimiter + entityPath;
 
             // Attach protocol and the host delimiter
-            if (entity->Protocol.size() > 0)
+            if (entity->Protocol.length() > 0)
                 entityPath = entity->Protocol + ProtocolDelimiter + HostDelimiter + entityPath;
-            else if (hostSection.size() > 0)
+            else if (hostSection.length() > 0)
                 entityPath = HostDelimiter + entityPath;
 
             // Attach the entity to the final result
-            if (result.size() == 0)
+            if (result.length() == 0)
                 result = entityPath;
             else result += EntityDelimiter + entityPath;
         }
